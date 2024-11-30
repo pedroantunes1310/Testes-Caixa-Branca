f77ce3fd3b9e9aec59ad336d2058fce1fcc1fbd5
Pedro Antunes <saruehordep@gmail.com>
2024-11-30 00:46:21 -0300

    Etapa 4
    Correção do código inicial, aplicando as devidas validações e tratamento de exceções. Implementação dos comentários junto ao JavaDoc e inserção dos arquivos na pasta docs.

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Login","c":"User","l":"conectarBD()"},{"p":"Login","c":"User","l":"nome"},{"p":"Login","c":"User","l":"result"},{"p":"Login","c":"User","l":"User()","u":"%3Cinit%3E()"},{"p":"Login","c":"User","l":"verificarUsuario(String, String)","u":"verificarUsuario(java.lang.String,java.lang.String)"}];updateSearchResults();
